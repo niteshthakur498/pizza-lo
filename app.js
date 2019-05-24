33025ff05afecb72edfe979d26ee802c55f4be3b
Nitesh Thakur <niteshthakur498@gmail.com>
2019-05-25 01:15:15 +0530

    Path Reformed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/api/orders',Orders);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('../frontent/build'));
     app.get('*', (req,res)=>{
-        res.sendFile(path.join(__dirname,'../frontend','build', 'index.html'));
+        res.sendFile(path.join(__dirname,'/frontend','build', 'index.html'));
         
     });
 }
